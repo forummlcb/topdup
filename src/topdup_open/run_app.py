18e6ecddc2c22c516ab3c98f1696474d5ec72c42
Alex Nguyen <alexvn.edu@gmail.com>
2021-01-03 10:42:19 -0500

    add  help description

--- src/topdup_open/run_app.py
+++ src/topdup_open/run_app.py
@@ -2,11 +2,12 @@ import argparse
 from topdup_app import app
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="My parser")
+    parser = argparse.ArgumentParser()
     parser.add_argument(
         "-d",
         "--debug", 
-        action='store_true'
+        action='store_true',
+        help="turn on debugging mode"
     )
     args = parser.parse_args()
     if args.debug:
