8bb9ae412edc73acf277ccd722fef86c5aa880f8
Alex Nguyen <alexvn.edu@gmail.com>
2021-01-03 10:37:50 -0500

    add argparse debugging argument

--- src/topdup_open/run_app.py
+++ src/topdup_open/run_app.py
@@ -1,5 +1,16 @@
+import argparse
 from topdup_app import app
 
 if __name__ == "__main__":
-    app.run()
+    parser = argparse.ArgumentParser(description="My parser")
+    parser.add_argument(
+        "-d",
+        "--debug", 
+        action='store_true'
+    )
+    args = parser.parse_args()
+    if args.debug:
+        app.run(debug=True)
+    else:
+        app.run()
     # app.run(host='0.0.0.0', debug=True) #docker
