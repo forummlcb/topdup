3e5e46093e2a318a4f4b4cda12258906d9ade1fe
Cong Cuong PHAM <ccuong.ph@gmail.com>
2021-01-26 09:46:35 +0100

    update(similarityReports): update get all query

--- modules/web_app/backend/src/controllers/similarity-report.js
+++ modules/web_app/backend/src/controllers/similarity-report.js
@@ -1,16 +1,27 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
   user: 'admin',
-  host: 'localhost',
+  host: '3.1.100.54',
   database: 'topdup_db',
-  password: 'admin',
+  password: 'uyL7WgydqKNkNMWe',
   port: 5432
 });
 
 const getSimilarityRecords = (request, response) => {
   const query = `
-        SELECT *
-        FROM public."similarity_report"
+      SELECT  article_a.title as article_a_title, 
+              article_a.domain as article_a_domain,
+              article_a.author as article_a_author,
+              article_a.last_updated_date as article_a_last_updated_date,
+              s_report.sim_score as sim_score,
+              article_b.title as article_b_title, 
+              article_b.domain as article_b_domain,
+              article_b.author as article_b_author,
+              article_b.last_updated_date as article_b_last_updated_date
+      FROM public."similarity_report" as s_report
+      INNER JOIN public."article" as article_a ON article_a.id = s_report.article_a_id
+      INNER JOIN public."article" as article_b ON article_b.id = s_report.article_b_id    
+      ORDER BY sim_score DESC;
     `;
   pool.query(query, (error, results) => {
     if (error) {
