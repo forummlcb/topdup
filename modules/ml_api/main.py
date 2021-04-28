528d21cc8a33ba94dbe4a7dc0fe62fafa88043b2
nammotd <nguyenhainam.fetel@gmail.com>
2021-04-28 15:08:28 +0700

    Update drone file

--- modules/ml_api/main.py
+++ modules/ml_api/main.py
@@ -178,5 +178,4 @@ def http_exception_handler(request, exc):
 if __name__ == "__main__":
     # For debugging only
     import uvicorn
-
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
